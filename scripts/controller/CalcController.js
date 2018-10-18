3e8b73282f1f85ed53b4486b5994d59fe221ad34
arthurabreu00 <arthurabreu00@gmail.com>
2018-10-18 16:56:02 -0300

    Calculando

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -5,6 +5,8 @@ class CalcController {
         this.display = document.querySelector(display);
         this.buttons = document.querySelectorAll(buttons);
         this.initEvents();
+        this.contSimb = 0;
+        this.verificar = "";
     }
 
 
@@ -19,32 +21,53 @@ class CalcController {
 
     txtButton(txt){
 
+
         if(!isNaN(txt))
             this.display.value += txt;
         else{
-            switch (txt) {
-                case '%':
-                    break;
-                case '.':
-                    if(this.display.value.length == 0){
-                        this.display.value += 0 + txt;
-                    }else{
-                        this.display.value += txt;
+            this.contSimb++;
+                switch (txt) {
+                    case '%':
+                        break;
+                    case '.':
+                        if(this.display.value.length == 0){
+                            this.display.value += 0 + txt;
+                        }else{
+                            this.display.value += txt;
+                        }
+                        break;
+                    case 'X':
+                        this.display.value += '*';
+                        break;
+                    case '=':
+                        this.calculo();
+                        break;
+                    case 'x²':{
+                        this.display.value += this.display.value * this.display.value;
                     }
-                    break;
-                case 'x²':
-                    break;
-                case 'CE':
-                    this.display.value = "";
-                    break;
-                case '←':
-                    this.display.value = this.removeOneCaract();
-                    break;
-                default:
-                    this.display.value += txt;
-                    break;
-            }
+                    case '÷':
+                        this.display.value += '/';
+                        break;
+                    case 'CE':
+                        this.display.value = "";
+                        break;
+                    case '←':
+                        this.display.value = this.removeOneCaract();
+                        break;
+                    default:
+                        this.display.value += txt;
+                        break;
+                }
+            
+           
         }
+    }
+
+    calculo(){
+        let calc = eval(this.display.value);
+        this.display.value = calc;
+        this.contSimb = 0;
+        this.verificar = '';
     }
 
     removeOneCaract(){
